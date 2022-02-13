4f20739b3c1ea205fc02f58ffc38e5c8aab29e7e
shankhadeep.it@gmail.com <shankhadeep.it@gmail.com>
2022-02-14 01:20:04 +0530

    updated pom and unit tests

--- src/test/java/com/trade/trader/resource/TraderScheduledJobTest.java
+++ src/test/java/com/trade/trader/resource/TraderScheduledJobTest.java
@@ -3,16 +3,23 @@ package com.trade.trader.resource;
 import com.trade.trader.TraderApplication;
 import org.awaitility.Duration;
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.junit4.SpringRunner;
 
 
 import static org.awaitility.Awaitility.await;
-import static org.mockito.Mockito.atLeast;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 
-@SpringJUnitConfig(TraderApplication.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource("classpath:test.properties")
 public class TraderScheduledJobTest {
     @SpyBean
     private TraderScheduledJob traderScheduledJob;
@@ -20,8 +27,8 @@ public class TraderScheduledJobTest {
     @Test
     public void checkSchedulerToUpdateExpiryFlag(){
         await()
-                .atMost(Duration.TWO_MINUTES)
-                .untilAsserted(()->verify(traderScheduledJob,atLeast(2)).updatingMaturityDate());
+                .atMost(Duration.ONE_MINUTE)
+                .untilAsserted(()->verify(traderScheduledJob,times(1)).updatingMaturityDate());
     }
 
 }
